b9e157943acc9920e81680b592569415ca34c9a2
bludere <142813826+bludere@users.noreply.github.com>
2023-09-17 18:35:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function mute() { 
+    document.getElementById("background_audio").muted = true;
+  } 
+  
+  function unmute() { 
+    document.getElementById("background_audio").muted = false;
+  }
